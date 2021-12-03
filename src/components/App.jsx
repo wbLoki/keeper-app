bfabec3924b9995f6bf8c69eac3c25ce77b77c25
wakishi <hun73r@icloud.com>
2021-12-03 12:03:21 +0100

    added function to the add button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,28 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function addNote(note){
+        setNotes(prevItems => {
+            return [...prevItems, note];
+          });
+    }
+    
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea 
+          addNote={addNote}
+      />
+      {notes.map((note, index) => (
+          <Note 
+              key={index}
+              id={index}
+              title={note.title}
+              content={note.content}
+          />
+      ))}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+    const [inputText, setText] = useState({
+        title:"",
+        content:""
+    });
+    function handleChange(e){
+        const target = e.target.name;
+        target === "title" ? setText({...inputText, title: e.target.value}) :
+        setText({...inputText, content: e.target.value});
+        // console.log(inputText);
+    }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title" placeholder="Title"
+        value={inputText.title}
+        onChange={handleChange}/>
+        <textarea name="content"
+        placeholder="Take a note..." rows="3"
+        value={inputText.content}
+        onChange={handleChange}/>
+        <button onClick={(e) => {
+        e.preventDefault()
+        props.addNote(inputText)
+        setText({
+        title:"",
+        content:""
+        })
+        }}>Add</button>
       </form>
     </div>
   );
